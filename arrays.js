038dc04d21664faa4899241075d4c6ee45a1e841
Sena Heydari <sheydari@dosomething.org>
2020-05-05 01:25:45 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,25 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'];
+
+function addElementToBeginningOfArray(array, element) {
+  var new_array = [element, ...array];
+  return new_array;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  var new_array = [...array, element];
+  return new_array;
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
